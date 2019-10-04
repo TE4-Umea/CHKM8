1a4150acda71eb1cb759afe71b046e08b6100aa2
Aborren <elias.renman00@gmail.com>
2019-10-04 10:46:32 +0200

    Created Payload model

---
+++ models/Payload.js
@@ -0,0 +1,14 @@
+class Payload {
+    constructor(req) {
+        this.token = req.body.token;
+        this.check_in = req.body.check_in;
+        this.project = req.body.project ? req.body.project : null;
+        this.username = req.body.username;
+        this.password = req.body.password;
+        this.name = req.body.name;
+        this.sign_token = req.body.sign_token;
+
+    }
+}
+
+module.exports = Payload;
