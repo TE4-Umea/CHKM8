cbd5631ff1c7d7e7ffe179c56b6277b659bf1e0e
Aborren <elias.renman00@gmail.com>
2019-10-07 09:06:20 +0200

    Refractored webhook into controller and route

--- routes/WebhookRoutes.js
+++ routes/WebhookRoutes.js
@@ -1,19 +1,14 @@
 class WebhookRoutes {
     constructor(server) {
+
+        let Controller = new (require('../controllers/WebHookController'))();
         /**
          * Github WEBHOOK
          * Change config:branch to false if you wish to disable the webhook.
          * The webhook does not confirm that it comes from github, so it's important to disable it for release.
          * TODO: Verify webhook request is from Github
          */
-        server.app.post('/webhook', async (req, res) => {
-            if (server.config.branch) {
-                require('child_process').exec(
-                    'git pull origin ' + server.config.branch + ' --no-edit'
-                );
-                server.log('Reloading via webhook');
-            }
-        });
+        server.app.post('/webhook', Controller.index);
     }
 }
 
