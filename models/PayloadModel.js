afc760547aa247e773bd0190cd4793ccaad1d34e
Olle Kaiser <olle.kaiser@learnet.se>
2019-10-09 15:35:48 +0200

    ree

--- models/PayloadModel.js
+++
@@ -1,9 +0,0 @@
-class PayloadModel {
-    constructor(req) {
-        for(var key in req.body) this[key] = req.body[key];
-        for(key in req.query) this[key] = req.query[key];
-        return this;
-    }
-}
-
-module.exports = PayloadModel;
