22db3c09f6ce995b8c8c8e77d44419bb7e7b237b
luddoro <luddenr1@live.se>
2019-10-10 13:09:13 +0200

    Canvas Responsiveness

--- cdn/js/dashboard.js
+++ cdn/js/dashboard.js
@@ -15,15 +15,9 @@ if (sign_token) {
         });
 }
 
-
-
 const mock_data = [ { "id": 1, "user": 9, "time": 1568272930552, "check_in": 1, "type": "web", "original_time": 1568272930552 }, { "id": 2, "user": 9, "time": 1568287231086, "check_in": 0, "type": "web", "original_time": 1568287231086 }, { "id": 20, "user": 9, "time": 1568318933943, "check_in": 0, "type": "web", "original_time": 1568318924047 }, { "id": 22, "user": 9, "time": 1568319132512, "check_in": 0, "type": "web", "original_time": 1568319131079 }, { "id": 24, "user": 9, "time": 1568320102649, "check_in": 0, "type": "web", "original_time": 1568320096003 }, { "id": 28, "user": 9, "time": 1568321029810, "check_in": 0, "type": "web", "original_time": 1568321010101 }, { "id": 32, "user": 9, "time": 1568321086190, "check_in": 0, "type": "web", "original_time": 1568321083239 }, { "id": 39, "user": 9, "time": 1568355601619, "check_in": 0, "type": "web", "original_time": 1568355585119 }, { "id": 45, "user": 9, "time": 1568357475449, "check_in": 1, "type": "web", "original_time": 1568357475448 }, { "id": 48, "user": 9, "time": 1568359923048, "check_in": 1, "type": "web", "original_time": 1568359866435 }, { "id": 50, "user": 9, "time": 1568360494922, "check_in": 0, "type": "web", "original_time": 1568360494921 }, { "id": 71, "user": 9, "time": 1568362819972, "check_in": 1, "type": "web", "original_time": 1568362819971 }, { "id": 73, "user": 9, "time": 1568365720248, "check_in": 1, "type": "web", "original_time": 1568365719352 }, { "id": 74, "user": 9, "time": 1568368496137, "check_in": 0, "type": "web", "original_time": 1568368496136 }, { "id": 77, "user": 9, "time": 1568371402882, "check_in": 1, "type": "web", "original_time": 1568371402882 }, { "id": 79, "user": 9, "time": 1568371795371, "check_in": 1, "type": "web", "original_time": 1568371793678 }, { "id": 81, "user": 9, "time": 1568374086562, "check_in": 1, "type": "web", "original_time": 1568374084965 }, { "id": 87, "user": 9, "time": 1568375936546, "check_in": 1, "type": "web", "original_time": 1568375930785 }, { "id": 89, "user": 9, "time": 1568377355940, "check_in": 1, "type": "web", "original_time": 1568377354785 }, { "id": 91, "user": 9, "time": 1568377765315, "check_in": 1, "type": "web", "original_time": 1568377762288 }, { "id": 94, "user": 9, "time": 1568378322861, "check_in": 1, "type": "web", "original_time": 1568378321894 }, { "id": 96, "user": 9, "time": 1568381310040, "check_in": 0, "type": "web", "original_time": 1568381310038 }, { "id": 100, "user": 9, "time": 1568382760410, "check_in": 0, "type": "web", "original_time": 1568382759474 }, { "id": 101, "user": 9, "time": 1568614297278, "check_in": 1, "type": "web", "original_time": 1568614297277 }, { "id": 113, "user": 9, "time": 1568615863139, "check_in": 1, "type": "web", "original_time": 1568615859882 }, { "id": 121, "user": 9, "time": 1568615970424, "check_in": 1, "type": "web", "original_time": 1568615960831 }, { "id": 128, "user": 9, "time": 1568627214677, "check_in": 0, "type": "card", "original_time": 1568627214674 }, { "id": 131, "user": 9, "time": 1568635634263, "check_in": 1, "type": "web", "original_time": 1568635634261 }, { "id": 136, "user": 9, "time": 1568702789299, "check_in": 0, "type": "web", "original_time": 1568702789297 }, { "id": 139, "user": 9, "time": 1568703987228, "check_in": 1, "type": "web", "original_time": 1568703987226 }, { "id": 152, "user": 9, "time": 1568729974138, "check_in": 0, "type": "card", "original_time": 1568729974135 }, { "id": 153, "user": 9, "time": 1568787516529, "check_in": 1, "type": "card", "original_time": 1568787516526 }, { "id": 163, "user": 9, "time": 1568793696906, "check_in": 1, "type": "web", "original_time": 1568793695605 }, { "id": 165, "user": 9, "time": 1568794073926, "check_in": 1, "type": "web", "original_time": 1568794073059 }, { "id": 168, "user": 9, "time": 1568821819711, "check_in": 0, "type": "card", "original_time": 1568821819708 }, { "id": 169, "user": 9, "time": 1568873504233, "check_in": 1, "type": "card", "original_time": 1568873504230 }, { "id": 177, "user": 9, "time": 1568960152571, "check_in": 1, "type": "card", "original_time": 1568960137099 }, { "id": 185, "user": 9, "time": 1568994802705, "check_in": 0, "type": "card", "original_time": 1568994802702 }, { "id": 186, "user": 9, "time": 1569219186712, "check_in": 1, "type": "card", "original_time": 1569219186709 }, { "id": 191, "user": 9, "time": 1569232076306, "check_in": 0, "type": "card", "original_time": 1569232076303 }, { "id": 192, "user": 9, "time": 1569244798002, "check_in": 1, "type": "card", "original_time": 1569244797998 }, { "id": 193, "user": 9, "time": 1569253712484, "check_in": 0, "type": "card", "original_time": 1569253712481 }, { "id": 197, "user": 9, "time": 1569305921812, "check_in": 1, "type": "card", "original_time": 1569305921808 }, { "id": 200, "user": 9, "time": 1569320415356, "check_in": 0, "type": "card", "original_time": 1569320415352 }, { "id": 202, "user": 9, "time": 1569337051385, "check_in": 1, "type": "card", "original_time": 1569337051380 }, { "id": 204, "user": 9, "time": 1569337114762, "check_in": 0, "type": "card", "original_time": 1569337114758 }, { "id": 221, "user": 9, "time": 1570107386566, "check_in": 1, "type": "web", "original_time": 1570107359788 } ];
 
-
-
 var days
-
-
 
 function format_days(h) {
     if (h.length == 0) {
@@ -152,7 +146,6 @@ function render_history() {
     }
 }
 
-
 CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
     if (w < 2 * r) r = w / 2;
     if (h < 2 * r) r = h / 2;
@@ -165,19 +158,6 @@ CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
     this.closePath();
     return this;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 var time_el = document.getElementById('time');
 var sec_bar = document.getElementById('seconds-bar');
@@ -194,6 +174,11 @@ function force_length(val) {
     return val.toString().length == 2 ? val.toString() : '0' + val.toString();
 }
 
+update_clock();
+setInterval(() => {
+    update_clock();
+}, 500);
+
 on_login = () => {
     if (me.slack_id) document.getElementById('slack-button').remove();
     update_checked_in_status(me.checked_in);
@@ -203,15 +188,15 @@ on_login = () => {
     document.getElementById('logged-in-as').innerText =
         'Logged in as ' + me.name + ' (' + me.username + ')';
     insert_projects();
-
-    format_days(mock_data)
 };
 
 /**
  * Creates a project card
  */
 function insert_projects() {
-    document.getElementById('projects').innerHTML = generate_project_cards(me.projects);
+    document.getElementById('projects').innerHTML = generate_project_cards(
+        me.projects
+    );
     for (el of document.getElementsByClassName('project')) {
         light_up_project(el, false, false);
     }
@@ -239,12 +224,11 @@ function generate_project_card(project) {
     //TODO
     return `<div class="project" hover="false" project-name="${
         project.name
-    }"><div class='project-upper-row'><span class="project-name">${project.name.toUpperCase()}</span><span id='${project.name + "_time"}'>${active_project_time()}</div><canvas class="project-timeline"></canvas><button class="project-button mdc-button button--active" onclick="check_in_project('${
+    }"><div class='project-upper-row'><span class="project-name">${project.name.toUpperCase()}</span><span id='${project.name +
+        '_time'}'>${active_project_time()}</div><canvas class="project-timeline"></canvas><button class="project-button mdc-button button--active" onclick="check_in_project('${
         project.name
     }')">${
-        me.checked_in_project == project.name
-            ? get_button_text()
-            : 'check in'
+        me.checked_in_project == project.name ? get_button_text() : 'check in'
     }</button></div>`;
 }
 
@@ -252,7 +236,162 @@ function generate_project_card(project) {
  * @returns will return a string of the corrent working time.
  */
 function active_project_time() {
-    return "0:00";
+    return '0:00';
+}
+
+/**
+ * Reloads the base page elements
+ */
+function reload_dash() {
+    //Gets user data specified by token.
+    axios
+        .get('/api/user', {
+            params: {
+                token,
+            },
+        })
+        .then(res => {
+            var data = res.data;
+            if (data.success) {
+                me = data.profile;
+                insert_projects();
+            }
+        });
+}
+
+var hovering = false;
+document.addEventListener('mousemove', e => {
+    if (me) {
+        var hover;
+        for (var el of e.composedPath()) {
+            if (el.getAttribute) {
+                if (el.getAttribute('project-name')) {
+                    hover = el;
+                    if (el.getAttribute('hover') == 'false') {
+                        el.setAttribute('hover', true);
+                        light_up_project(el);
+                        hovering = true;
+                    }
+                }
+            }
+        }
+
+        if (hovering) {
+            var hovering_found = false;
+            for (var el of document.getElementsByClassName('project')) {
+                if (
+                    me.checked_in_project != el.getAttribute('project-name') &&
+                    hover != el
+                ) {
+                    hovering_found = true;
+                    el.setAttribute('hover', false);
+                    light_up_project(el, false, false);
+                } else {
+                    hovering_found = true;
+                }
+            }
+            if (!hovering_found) {
+                /* if(interval) clearInterval(interval) */
+                hovering = false;
+            }
+        }
+    }
+});
+
+function check_in() {
+    //Checks in user.
+    axios
+        .post('/api/user/check', {
+            token: token,
+        })
+        .then(res => {
+            var data = res.data;
+            me.checked_in = data.checked_in;
+            me.checked_in_time = 0;
+            notice(data.text, data.success);
+            if (!data.checked_in) me.checked_in_project = '';
+            update_projects(data.checked_in);
+            update_checked_in_status(data.checked_in);
+        });
+}
+
+var time_el = document.getElementById('time');
+var sec_bar = document.getElementById('seconds-bar');
+
+function update_clock() {
+    var time = new Date();
+    time_el.innerText =
+        force_length(time.getHours()) + ':' + force_length(time.getMinutes());
+    sec_bar.style.width =
+        ((time.getSeconds() + time.getMilliseconds() / 1000) / 60) * 100 + '%';
+}
+
+function force_length(val) {
+    return val.toString().length == 2 ? val.toString() : '0' + val.toString();
+}
+
+on_login = () => {
+    if (me.slack_id) document.getElementById('slack-button').remove();
+    update_checked_in_status(me.checked_in);
+    document.getElementById('avatar').src = me.avatar
+        ? me.avatar
+        : 'img/avatar.png';
+    document.getElementById('logged-in-as').innerText =
+        'Logged in as ' + me.name + ' (' + me.username + ')';
+    insert_projects();
+
+    format_days(mock_data);
+};
+
+/**
+ * Creates a project card
+ */
+function insert_projects() {
+    document.getElementById('projects').innerHTML = generate_project_cards(
+        me.projects
+    );
+    for (el of document.getElementsByClassName('project')) {
+        light_up_project(el, false, false);
+    }
+
+    update_projects(me.checked_in, me.checked_in_project);
+}
+
+/**
+ * Generates a group of cards as html.
+ * @param {array} projects json file of all projects.
+ */
+function generate_project_cards(projects) {
+    var html = '';
+    for (var project of projects) {
+        html += generate_project_card(project);
+    }
+    return html;
+}
+
+/**
+ * Generates html for a projects card.
+ * @param {Object} project project object containing project details.
+ */
+function generate_project_card(project) {
+    //TODO
+    return `<div class="project" hover="false" project-name="${
+        project.name
+    }"><div><div class='project-upper-row'><span class="project-name">${
+        project.name
+    }</span><span id='${project.name +
+        '_time'}'>${active_project_time()}</div><canvas class="project-timeline"></canvas></div><button class="project-button mdc-button button--active" onclick="check_in_project('${
+        project.name
+    }')">${
+        me.checked_in_project == project.name ? get_button_text() : 'check in'
+    }</button></div>`;
+}
+
+/**
+ * @returns will return a string of the corrent working time.
+ */
+function active_project_time() {
+    return '0:00';
 }
 
 /**
@@ -428,11 +567,11 @@ function light_up_project(el, light_up = true, animate = true) {
                 clearInterval(interval);
                 return;
             }
-            render_canvas(el.children[1], project, progress);
+            render_canvas(el.getElementsByClassName("project-timeline")[0], project, progress);
             progress += light_up ? speed : -speed;
         }, 30);
     } else {
-        render_canvas(el.children[1], project, light_up ? 1 : 0);
+        render_canvas(el.getElementsByClassName("project-timeline")[0], project, light_up ? 1 : 0);
     }
 }
 
@@ -475,7 +614,7 @@ function check_in_project(project_name) {
 function update_projects(checked_in, project_name) {
     for (var el of document.getElementsByClassName('project')) {
         render_canvas(
-            el.children[1],
+            el.getElementsByClassName("project-timeline")[0],
             get_project_from_name(el.getAttribute('project-name'))
         );
         if (el.getAttribute('project-name') == me.checked_in_project) {
@@ -483,9 +622,9 @@ function update_projects(checked_in, project_name) {
         }
         if (el.getAttribute('project-name') != project_name) {
             light_up_project(el, false, false);
-            el.children[2].innerText = 'Check in';
+            el.getElementsByClassName("project-button")[0].innerText = 'Check in';
         } else {
-            el.children[2].innerText = get_button_text();
+            el.getElementsByClassName("project-button")[0].innerText = get_button_text();
         }
     }
     update_checked_in_status(checked_in);
@@ -504,9 +643,7 @@ function update_checked_in_status(checked_in) {
 }
 
 function get_button_text() {
-    return me.checked_in
-        ? 'CHECK OUT'
-        : 'CHECK IN';
+    return me.checked_in ? 'CHECK OUT' : 'CHECK IN';
 }
 
 function format_time(ms) {
@@ -531,7 +668,6 @@ function new_project() {
             });
     }
 }
-
 
 update_clock();
 setInterval(() => {
